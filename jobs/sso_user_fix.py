80c694fbeb2d4843cd430896da5bbec4c9d185aa
Federico Tabbò <federico.tabbo@networktocode.com>
2026-08-22 10:07:01 +0200

    first commit

---
+++ jobs/sso_user_fix.py
@@ -0,0 +1,36 @@
+from nautobot.apps.jobs import Job, register_jobs
+from nautobot.users.models import User
+
+class SSOUserFix(Job):
+    class Meta:
+        name = "Associate local users with SSO users in case of duplicates"
+
+    def deduplicate(self, nonce_user: User):
+        try:
+            real_user = User.objects.get(username=nonce_user.username[:-16])
+        except User.DoesNotExist:
+            self.logger.failure(f"Base user not found for {nonce_user.username}", extra={"object": nonce_user})
+
+        social_auth = nonce_user.social_auth.all()
+
+        for x in social_auth:
+            s.user = real_user
+            s.save()
+
+        nonce_user.delete()
+
+        self.logger.success(f"Updated SSO login for {real_user.username}", extra={"object": real_user})
+
+
+
+    def run(self):
+        self.logger.info("Searching for users")
+
+        duplicate_users = User.objects.filter(username__regex=".*@.*\\..*[0-9a-f]{16}")
+
+        for user in duplicate_users:
+            self.deduplicate(user)
+
+
+
+register_jobs(SSOUserFix)
